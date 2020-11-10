edb68bdc07047f0783b3b491b998c4fa5e7316e6
Jason Taylor <jason@jasongtaylor.com>
2020-11-10 02:13:59 -0500

    Adding ubuntu kubernetes

--- build.sh
+++ build.sh
@@ -20,4 +20,12 @@ cd ..
 vagrant box remove ubuntu-base
 vagrant box add ubuntu-base boxes/ubuntu-base.box
 
+echo "Building Kubernetes Ubuntu Vagrant Box"
+cd ubuntu-k8s
+vagrant up
+vagrant package --output ../boxes/ubuntu-k8s.box
+cd ..
+vagrant box remove ubuntu-k8s
+vagrant box add ubuntu-k8s boxes/ubuntu-k8s.box
+
 echo "Completed building Vagrant base boxes"
