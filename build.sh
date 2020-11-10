2b0976ca72925b92f8333a01b3da4d750ba19df3
Jason Taylor <jason@jasongtaylor.com>
2020-11-10 01:03:45 -0500

    Add Ubuntu Base

---
+++ build.sh
@@ -0,0 +1,18 @@
+echo "Building Vagrant base boxes"
+
+echo "Setup building area"
+if [ -d "boxes" ]; then
+  rm -rf boxes
+if
+
+mkdir boxes
+
+echo "Building Base Ubuntu Vagrant Box"
+cd ubuntu-base
+vagrant up
+vagrant package --output ../boxes/ubuntu-base.box
+cd ..
+vagrant box remove ubuntu-base
+vagrant box add ubuntu-base boxes/ubuntu-base.box
+
+echo "Completed building Vagrant base boxes"
